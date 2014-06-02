d18726131e8dce1afa270cf7862e1aa6fce66d40
Ondřej Garncarz <ondrej@garncarz.cz>
2014-06-03 00:35:22 +0200

    Input/output groups of neurons.

--- spiking.py
+++ spiking.py
@@ -15,12 +15,17 @@ class Net:
 		
 		self.tresholds = rnd.random_sample(self.neuronCount)
 		# self.values = np.zeros(self.neuronCount)
-		self.values = rnd.random_sample(self.neuronCount)
+		self.values = rnd.random(self.neuronCount)
 		
 		self.groups = rnd.randint(self.groupCount, size = self.neuronCount)
+		self.inputs = filter(lambda neuron: self.groups[neuron] == 0,
+			range(self.neuronCount))
+		self.outputs = filter(lambda neuron:
+			self.groups[neuron] == self.groupCount - 1, range(self.neuronCount))
 
 	
 	def step(self):
+		self.randomInputs()
 		self.valuesNext = np.copy(self.values)
 		
 		for neuron in range(self.neuronCount):
@@ -33,4 +38,14 @@ class Net:
 			self.valuesNext[neuron] -= self.values[neuron]
 		
 		self.values = self.valuesNext
+
+	def randomInputs(self):
+		for neuron in self.inputs:
+			self.values[neuron] = rnd.random()
+	
+	
+	def printOutputs(self):
+		print(", ".join(map(lambda neuron: str(self.values[neuron]),
+			self.outputs)))
+	
 
